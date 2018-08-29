386c9c137a97263db84609e30c76c4817d57e73a
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-29 08:55:58 +0530

    a

--- reactiveforms/src/app/customers/customer.component.ts
+++ reactiveforms/src/app/customers/customer.component.ts
@@ -1,8 +1,29 @@
 import { Component, OnInit} from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder,
+         Validators,
+         ValidatorFn,
+         AbstractControl} from '@angular/forms';
 
 import { Customer } from './customer';
 
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return{'range': true};
+        }
+        return null;
+    };
+}
+
+function emailMatcher(c: AbstractControl) {
+    const emailControl = c.get('email');
+    const confrimControl = c.get('confrimEmail');
+    if (emailControl.value === confrimControl.value) {
+        return null;
+    } else {
+        return {'match': true};
+    }
+}
 @Component({
     selector: 'my-signup',
     templateUrl: './customer.component.html'
@@ -16,9 +37,15 @@ export class CustomerComponent implements OnInit  {
 
     ngOnInit(): void {
         this.customerForm = this.fb.group({
-            firstName: [''],
-            lastName: [''],
-            email: ['']
+            firstName: ['', [Validators.required, Validators.minLength(5)]],
+            lastName: ['', [Validators.required, Validators.maxLength(6)]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern('[0-9a-zA-Z]+@[a-zA-Z0-9]+')]],
+                confrimEmail: ['', [Validators.required]],
+            }, {validator: emailMatcher}),
+            phone: [],
+            notification : 'email',
+            rating: ['', ratingRange(1, 8)]
         });
     }
     populateData(): void {
@@ -33,6 +60,19 @@ export class CustomerComponent implements OnInit  {
             firstName: fname.charAt(0).toUpperCase() + fname.slice(1)
         });
     }
+
+    setNotfication(notifyVia: string): void {
+        console.log(notifyVia);
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'phone') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+
+        phoneControl.updateValueAndValidity();
+    }
+
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
